dce633e2044ff8f088ea49fa44c8eb67f8887280
Himanshu-KRE <himanshu.nag@probestack.io>
2026-09-02 10:21:50 +0530

    Update generated SDK file: src/main/java/com/probestack/sdk/ServerVariable.java

--- src/main/java/com/probestack/sdk/ServerVariable.java
+++ src/main/java/com/probestack/sdk/ServerVariable.java
@@ -18,7 +18,7 @@ import java.util.HashSet;
 /**
  * Representing a Server Variable for server URL template substitution.
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-08-21T08:23:40.527625415Z[GMT]", comments = "Generator version: 7.9.0")public class ServerVariable {
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-09-02T04:51:02.135816264Z[GMT]", comments = "Generator version: 7.9.0")public class ServerVariable {
     public String description;
     public String defaultValue;
     public HashSet<String> enumValues = null;
